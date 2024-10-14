1916f4fb43897e155902408bd6c6c1026f26abb5
Ricardo Nunez <113212961+ricardonunez-io@users.noreply.github.com>
2024-10-14 10:39:03 -0700

    Add nodenext compatible extensions to dependencies of `openapi-parser`

--- packages/build-tooling/src/index.ts
+++ packages/build-tooling/src/index.ts
@@ -1,4 +1,4 @@
-export { findEntryPoints, addPackageFileExports } from './entry'
+export { findEntryPoints, addPackageFileExports } from './entry.js'
 export { createRollupConfig } from './rollup-options'
 export { alias, createViteBuildOptions, autoCSSInject } from './vite-options'
 export { ViteWatchWorkspace } from './workspace-reload'
